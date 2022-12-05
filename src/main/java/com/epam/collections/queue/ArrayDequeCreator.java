7f73dc18509112b7d874259104b926965c0586f0
Mikhail Konchyts <mixamtk@gmail.com>
2022-12-05 17:45:44 +0300

    resolve

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -5,6 +5,26 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
+
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        result.add(firstQueue.poll());
+        result.add(firstQueue.poll());
+        result.add(secondQueue.poll());
+        result.add(secondQueue.poll());
+        while (!firstQueue.isEmpty() & !secondQueue.isEmpty()) {
+                firstQueue.offer(result.pollLast());
+                if (firstQueue.size() >= 2)
+                    result.add(firstQueue.poll());
+                result.addLast(firstQueue.poll());
+
+            if (!secondQueue.isEmpty()) {
+                secondQueue.offer(result.pollLast());
+                if(secondQueue.size() >= 2)
+                    result.addLast(secondQueue.poll());
+                result.add(secondQueue.poll());
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,27 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> inList = new ArrayList<>();
+        if (numberOfDishes > 0) {
+            for (int i = 1; i <= numberOfDishes; i++)
+                inList.add(i);
+            int count = 0;
+            while (inList.size() > 1) {
+                count += everyDishNumberToEat;
+                if (count > inList.size()) {
+                    count -= inList.size() + 1;
+                } else
+                    count--;
+                result.add(inList.remove(count));
+            }
+            result.add(inList.get(0));
+        }
+
+        return result;
     }
 }
